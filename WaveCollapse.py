f93d7d3b5dba4aa3132f56a1778490d310320e6c
jconorgrogan <130090573+jconorgrogan@users.noreply.github.com>
2023-08-05 13:56:37 -0400

    Update WaveCollapse.py

--- WaveCollapse.py
+++ WaveCollapse.py
@@ -1,46 +1,60 @@
-# Complete code
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define a time array
-t = np.linspace(0, 1, 500)
+def number_properties_harmonic(number):
+    """Determine amplitude and phase based on a number with harmonic relationships."""
+    
+    # Prime factors (with repetition) extraction
+    prime_factors = []
+    i = 2
+    while i * i <= number:
+        if number % i:
+            i += 1
+        else:
+            number //= i
+            prime_factors.append(i)
+    if number > 1:
+        prime_factors.append(number)
+    
+    # Amplitude is the number of prime factors (with repetition)
+    amplitude = len(prime_factors)
+    
+    # Phase based on modulo 2pi
+    phase = number % (2 * np.pi)
+    
+    return amplitude, phase, prime_factors
 
-def number_to_waveform(number, rotation, amplitude, t):
-    """Translate a number in the Rotational Number System into a waveform."""
+def number_to_waveform_prime_influence(number, rotation, amplitude, prime_factors, t):
+    """Translate a number into a waveform influenced by its largest prime factor."""
     
-    # The frequency of the waveform corresponds to the number itself
-    freq = number
-    
-    # The phase of the waveform corresponds to the rotation
-    phase = rotation
-    
-    # The amplitude of the waveform corresponds to the amplitude
-    amplitude = amplitude
-    
-    # Generate the waveform
-    waveform = amplitude * np.sin(2 * np.pi * freq * t + phase)
+    # Base waveform for the largest prime factor
+    largest_prime = max(prime_factors)
+    waveform = amplitude * np.sin(2 * np.pi * largest_prime * t + rotation)
     
     return waveform
 
-def plot_waveform(number, rotation, amplitude, t):
-    """Generate and plot a waveform corresponding to a number in the Rotational Number System."""
+def plot_waveform_prime_influence(number, t):
+    """Generate and plot a waveform corresponding to a number influenced by its largest prime factor."""
+    
+    amplitude, rotation, prime_factors = number_properties_harmonic(number)
     
     # Generate the corresponding waveform
-    waveform = number_to_waveform(number, rotation, amplitude, t)
+    waveform = number_to_waveform_prime_influence(number, rotation, amplitude, prime_factors, t)
 
     # Plot the waveform
     plt.figure(figsize=(6, 4))
     plt.plot(t, waveform)
-    plt.title('Waveform corresponding to Number = {}, Rotation = {}, Amplitude = {}'.format(number, rotation, amplitude))
+    plt.title('Waveform for Number = {}, Rotation = {:.2f}, Amplitude = {}, Prime Factors = {}'.format(number, rotation, amplitude, prime_factors))
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
     plt.grid(True)
-    plt.show()
 
-# Choose a number, rotation, and amplitude
-number = 7
-rotation = np.pi/3
-amplitude = 2
+# Define a time array
+t = np.linspace(0, 1, 500)
 
-# Generate and plot the waveform
-plot_waveform(number, rotation, amplitude, t)
+# Test with various numbers
+numbers_to_test = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers_to_test:
+    plot_waveform_prime_influence(number, t)
+plt.tight_layout()
+plt.show()
