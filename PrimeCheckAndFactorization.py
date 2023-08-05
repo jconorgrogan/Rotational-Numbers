429e8c129109fe19d5ee7e6971bc795d6307a80d
jconorgrogan <130090573+jconorgrogan@users.noreply.github.com>
2023-08-05 12:00:32 -0400

    Update and rename PrimeCheck.py to PrimeCheckAndFactorization.py

--- PrimeCheckAndFactorization.py
+++ PrimeCheckAndFactorization.py
@@ -1,9 +1,11 @@
 from math import pi
 
-def rotate_overlap_check(n, tolerance=1e-9):
-    """Check if two nodes of n overlap with two nodes of any number from 2 to n-1 when rotated fully."""
+def rotation_overlap(n, tolerance=1e-9):
+    """Check if two nodes of n overlap with two nodes of any number from 2 to n-1 when rotated fully. 
+    Return the numbers for which this overlap occurs."""
     
     angles_n = [(2 * pi / n) * i for i in range(n)]
+    overlap_numbers = []
     
     for k in range(2, n):
         angles_k = [(2 * pi / k) * i for i in range(k)]
@@ -13,10 +15,11 @@ def rotate_overlap_check(n, tolerance=1e-9):
             overlaps = [angle for angle in angles_n if any(abs((angle + 2*pi*rotation/n) % (2*pi) - a) < tolerance for a in angles_k)]
             
             if len(overlaps) >= 2:
-                return True  # n is composite
+                overlap_numbers.append(k)
+                break  # Once overlap is found for a number k, no need to check further rotations
                 
-    return False  # n is prime
+    return overlap_numbers
 
 # Determine overlap behavior for numbers up to 50
-rotate_overlap_results = {n: rotate_overlap_check(n) for n in range(2, 51)}
-print(rotate_overlap_results)
+rotation_overlap_results = {n: rotation_overlap(n) for n in range(2, 51)}
+print(rotation_overlap_results)
