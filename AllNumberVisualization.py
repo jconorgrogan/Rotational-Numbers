514f1a79ba9e1151cc565e5049e4a2aef705d3ee
jconorgrogan <130090573+jconorgrogan@users.noreply.github.com>
2023-08-06 08:52:04 -0400

    Update AllNumberVisualization.py

--- AllNumberVisualization.py
+++ AllNumberVisualization.py
@@ -12,19 +12,21 @@ def is_prime(n):
             return False
     return True
 
-def determine_color_and_marker(n):
+def determine_color_and_marker(n, only_primes, only_composites):
+    if only_primes and not is_prime(n):
+        return None, None, None
+    if only_composites and is_prime(n):
+        return None, None, None
     if not is_prime(n):
-        return 'black', 's'  # s for square marker
+        return 'black', 's', node_size_composite  # s for square marker
     color_map = {2: 'red', 4: 'orange', 6: 'yellow', 8: 'green', 10: 'blue'}  # add more if needed
     for separation, color in color_map.items():
         if is_prime(n + separation) or is_prime(n - separation):
-            return color, 'o'  # o for circle marker
-    return 'purple', 'o'
+            return color, 'o', node_size_prime  # o for circle marker
+    return 'purple', 'o', node_size_prime
 
-def plot_intersecting_circles(integers, ax):
+def plot_intersecting_circles(integers, ax, only_primes=False, only_composites=False):
     max_integer = max(integers)
-    node_size_prime = 4
-    node_size_composite = 2
 
     for idx, integer in enumerate(integers):
         angles = np.linspace(0, 2 * np.pi, integer, endpoint=False)
@@ -33,9 +35,9 @@ def plot_intersecting_circles(integers, ax):
         y = radius * np.sin(angles)
         circle = plt.Circle((0, 0), radius, color='lightgray', fill=False)
         ax.add_artist(circle)
-        color, marker = determine_color_and_marker(integer)
-        node_size = node_size_prime if marker == 'o' else node_size_composite
-        ax.scatter(x, y, s=node_size, color=color, marker=marker)
+        color, marker, node_size = determine_color_and_marker(integer, only_primes, only_composites)
+        if color is not None:
+            ax.scatter(x, y, s=node_size, color=color, marker=marker)
 
     max_radius = len(integers) + 1
     ax.set_xlim(-max_radius, max_radius)
@@ -43,9 +45,12 @@ def plot_intersecting_circles(integers, ax):
     ax.set_aspect('equal')
     ax.axis('off')
 
-integers_to_plot_extended = generate_integers(500)
-fig, ax = plt.subplots(figsize=(40, 40))
-plot_intersecting_circles(integers_to_plot_extended, ax)
+node_size_prime = 20  # you can adjust this to change the size of the dots representing primes
+node_size_composite = 10  # you can adjust this to change the size of the dots representing composites
+
+integers_to_plot_extended = generate_integers(300)
+fig, ax = plt.subplots(figsize=(20, 20))
+plot_intersecting_circles(integers_to_plot_extended, ax, only_primes=True)  # set only_primes or only_composites to True as needed
 plt.title("RNS Integers", fontsize=30)
-plt.savefig("integers.jpg", dpi=800)
+plt.savefig("integers.jpg", dpi=400)
 plt.show()
