30d1de9a19da95b9edfa993aabaa48b88ac86c91
jconorgrogan <130090573+jconorgrogan@users.noreply.github.com>
2023-08-21 14:11:58 -0400

    Update discretecontinuous.py

--- Explorations/discretecontinuous.py
+++ Explorations/discretecontinuous.py
@@ -1,10 +1,39 @@
-//1. **Circle and Segment:** Consider a circle with radius \( r \) and a segment of length \( x \) where \( 0 \leq x \leq r \).
-//2. **Folding the Segment:** Fold the segment onto the circle, creating a right triangle with hypotenuse \( r \), adjacent side \( x \), and opposite side \( \sqrt{r^2 - x^2} \).
-//3. **Calculating the Angle:** The angle \( \theta \) in radians corresponding to the folded segment is given by
-//   \[
- //  \theta = \arccos\left(\frac{x}{r}\right).
-//   \]
-//4. **Representing Any Angle:** Since \( x \) can be any value between \( 0 \) and \( r \), \( \theta \) can be any value between \( 0 \) and \( \pi \).
+//1. 1. **Circle and Segment:** Consider a circle with radius \( r \) and a segment of length \( x \) where \( 0 \leq x \leq r \).
+2. **Orientation Constraint:** Assume that the fold corresponds to the first node to the top left (e.g., the node adjacent to the furthest right node on the circumference). This establishes a reference point and orientation for the system.
+3. **Folding the Segment:** Fold the segment onto the circle according to the defined orientation, creating a right triangle with hypotenuse \( r \), adjacent side \( x \), and opposite side \( \sqrt{r^2 - x^2} \).
+4. **Calculating the Angle:** The angle \( \theta \) in radians corresponding to the folded segment is given by
+   \[
+   \theta = \arccos\left(\frac{x}{r}\right).
+   \]
+5. **Representing Any Angle:** Since \( x \) can be any value between \( 0 \) and \( r \), \( \theta \) can be any value between \( 0 \) and \( \pi \).
+6. **Equidistant Nodes and Symmetry:** The number of equidistant nodes aligning with the origin and reference point depends on the symmetry of the angle \( \theta \). If \( \theta \) can be evenly divided into \( k \) parts that add up to \( 2\pi \), there will be \( k \) equidistant nodes.
+   \[
+   k = \frac{2\pi}{\theta}.
+   \]
+7. **Information Encoding through Angles and Nodes:** By selecting an angle \( \theta \) and corresponding nodes \( k \), a unique information pattern can be encoded. The continuous nature of \( \theta \) and the discrete nature of \( k \) allow for a multi-dimensional encoding space.
+8. **Area and Length Relationships:** The area of the sector and length of the arc corresponding to \( \theta \) are given by
+   \[
+   A = \frac{1}{2} r^2 \theta \quad \text{and} \quad L = r \theta.
+   \]
+9. **Inverse Relationship and Parametric Representation:** \( x \) and the point on the circle can be expressed as
+   \[
+   x = r \cos \theta \quad \text{and} \quad (r \cos \theta, r \sin \theta) = \left( x, \sqrt{r^2 - x^2} \right).
+   \]
+10. **New Computational Method Using Binary to Create Continuous**:
+   1. Initialize: \((x, y) = (0, 0)\), \(\theta = 0\), Binary = 11
+   2. Counting Zeros:
+      - Increment x by 1
+      - \(\theta = \arccos\left(\frac{x}{r}\right)\)
+   3. User Stops, Places a One:
+      - Record \((x, y) = \left(r\cos\theta, r\sin\theta\right)\)
+      - Binary += '1'
+   4. Counting Backwards:
+      - Decrement x by 1
+      - \(\theta = \arccos\left(\frac{x}{r}\right)\)
+   5. User Stops Again, Forms a Ratio:
+      - Ratio = \(\frac{\text{Final } \theta}{\text{Initial } \theta}\)
+   6. Interpretation and Encoding:
+      - Use Ratio, Binary, and Geometric Relationships
 
 
 import matplotlib.pyplot as plt
